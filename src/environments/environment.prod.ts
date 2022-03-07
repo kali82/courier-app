2beb2ad90b491fae3381d5275f9733bdb4fb47a6
mkaliszewski <mkaliszewski@ptc.com>
2022-03-07 14:56:01 +0100

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  serverUrl: 'http://localhost:4000/',
+  apiURL: 'http://localhost:4000/api/',
+  adminId: '5ead7ab5556feb3794d8b0a5',
 };
