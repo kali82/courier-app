cfe692dd36b292ef06d09427c244cb0420832fe7
mkalisz_96 <kaliszewskimaciej@gmail.com>
2022-03-14 00:48:12 +0100

    back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: 'http://localhost:4000/',
-  apiURL: 'http://localhost:4000/api/',
+  serverUrl: 'http://localhost:4500/',
+  apiURL: 'http://localhost:4500/api/',
   adminId: '5ead7ab5556feb3794d8b0a5',
 };
