08d77438c9ea37d6cf74b6f067b21ac56f486e79
mkalisz_96 <kaliszewskimaciej@gmail.com>
2022-03-21 17:46:27 +0100

    prod server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
-  serverUrl: 'http://localhost:4500/',
-  apiURL: 'http://localhost:4500/api/',
+  // serverUrl: 'http://localhost:4500/',
+  // apiURL: 'http://localhost:4500/api/',
+  serverUrl: 'http://83.20.68.88:20000/',
+  apiURL: 'http://83.20.68.88:20000/api/',
   adminId: '5ead7ab5556feb3794d8b0a5',
 };
