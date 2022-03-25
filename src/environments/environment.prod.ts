2174827cb27b7b431aabf4971e3869ff0a5aa5c6
mkaliszewski <mkaliszewski@ptc.com>
2022-03-25 14:51:53 +0100

    zxzx

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   // serverUrl: '/',
   // apiURL: 'api/',
   serverUrl: 'https://murmuring-hollows-26750.herokuapp.com',
-  apiURL: 'https://murmuring-hollows-26750.herokuapp.com/api/',
+  apiURL: 'https://murmuring-hollows-26750.herokuapp.com/api',
   adminId: '5ead7ab5556feb3794d8b0a5',
 };
